9e5133309eb7e9f4aaa43efb68601125b8473e1f
xuhao <bingoxuhao@gmail.com>
2021-09-24 19:05:10 +0800

    使用 Teleport

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,22 +1,24 @@
 <template>
   <template v-if="visible">
-    <div class="cat-dialog-overlay" 
-      @click="onClickOverlay"></div>
-    <div class="cat-dialog-wrapper">
-      <div class="cat-dialog">
-        <header>
-          <slot name="title"/>
-          <span class="cat-dialog-close" @click="close"></span>
-        </header>
-        <main>
-          <slot name="content"/>
-        </main>
-        <footer>
-          <Button level="main" @click="ok">OK</Button>
-          <Button @click="cancel">Cancel</Button>
-        </footer>
+    <Teleport to="#app">
+      <div class="cat-dialog-overlay" 
+        @click="onClickOverlay"></div>
+      <div class="cat-dialog-wrapper">
+        <div class="cat-dialog">
+          <header>
+            <slot name="title"/>
+            <span class="cat-dialog-close" @click="close"></span>
+          </header>
+          <main>
+            <slot name="content"/>
+          </main>
+          <footer>
+            <Button level="main" @click="ok">OK</Button>
+            <Button @click="cancel">Cancel</Button>
+          </footer>
+        </div>
       </div>
-    </div>
+    </Teleport>
   </template>
 </template>
 
