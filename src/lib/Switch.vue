d4338e3783bf962996adcf00e35788b702a98d42
xuhao <bingoxuhao@gmail.com>
2021-09-24 14:54:55 +0800

    CSS 最小影响原则

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,5 +1,5 @@
 <template>
-  <button @click="toggle" :class="{checked: value}"><span></span></button>
+  <button class="cat-switch" @click="toggle" :class="{'cat-checked': value}"><span></span></button>
 </template>
 <script lang="ts">
 import { ref } from 'vue'
@@ -15,10 +15,10 @@ export default {
   }
 }
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
 $h: 22px;
 $h2: $h - 4px;
-button {
+.cat-switch {
   height: $h;
   width: $h * 2;
   border: none;
@@ -35,7 +35,7 @@ button {
     border-radius: $h2 / 2;
     transition: all 250ms;
   }
-  &.checked {
+  &.cat-checked {
     background: #1890ff;
     > span {
       left: calc(100% - #{$h2} - 2px);
@@ -49,7 +49,7 @@ button {
       width: $h2 + 4px;
     }
   }
-  &.checked:active {
+  &.cat-checked:active {
     > span {
       width: $h2 + 4px;
       margin-left: -4px;
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
+import './lib/cat.scss'
+import './index.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
-import './index.scss'
 import { router } from './router'
 
 
