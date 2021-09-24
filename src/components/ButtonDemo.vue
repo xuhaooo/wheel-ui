14ef4d8d3e7dc2e9c82d652f22680a93825bb281
xuhao <bingoxuhao@gmail.com>
2021-09-24 13:54:30 +0800

    添加 theme 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -3,11 +3,20 @@
   <h1>实例 1</h1>
   <div>
     <Button>你好</Button>
+    <Button theme="button">你好</Button>
+    <Button theme="link">你好</Button>
+    <Button theme="text">你好</Button>
   </div>
 </template>
 <script lang="ts">
 import Button from '../lib/Button.vue'
 export default {
-  components: {Button}
+  components: {Button},
+  setup(){
+    const onClick = () => {
+      console.log('你好')
+    }
+    return {onClick}
+  }
 }
 </script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,20 @@
 <template>
-  <button>
+  <button class="cat-button" :class="`theme-${theme}`">
     <slot></slot>
   </button>
 </template>
+<script lang="ts">
+export default {
+  props: {
+    theme: {
+      type: String,
+      default: 'button'
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+  div {
+    border: 1px solid red;
+  }
+</style>
