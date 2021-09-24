cc5b73e73884aec343b9a78788cc633449939978
xuhao <bingoxuhao@gmail.com>
2021-09-24 20:55:53 +0800

    openDialog 一句话打开 Dialog

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -12,11 +12,16 @@
       <div>hi2</div>
     </template>
   </Dialog>
+  <h1>示例 2</h1>
+  <Button @click="showDialog">show</Button>
 </template>
+
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue'
 import Button from '../lib/Button.vue'
-import { ref } from 'vue'
+import { ref,h } from 'vue'
+import {openDialog} from '../lib/openDialog'
+
 export default {
   components: {Dialog, Button},
   setup(){
@@ -27,8 +32,17 @@ export default {
     const f1 = () => {
       return false
     }
-    const f2 = () => { }
-    return {x, toggle, f1, f2}
+    const f2 = () => {}
+
+    const showDialog = () => {
+      openDialog({
+          title: h('strong', {}, '标题'),
+          content: '你好',
+          ok(){console.log('ok')},
+          cancel(){console.log('cancel')}
+        })
+    }
+    return {x, toggle, f1, f2, showDialog}
   }
 }
 </script>
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -57,7 +57,7 @@ export default {
       }
     }
     const cancel = () => {
-      context.emit('cancel')
+      props.cancel && props.cancel()
       close()
     }
     return {close, onClickOverlay, ok, cancel}
