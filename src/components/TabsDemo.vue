9685d1ebba5a0b66472287ec434723530af0bbaf
xuhao <bingoxuhao@gmail.com>
2021-09-27 21:19:30 +0800

    创建 Tabs 和 Tab 组件

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,60 +1,15 @@
 <template>
-  <div>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-    <p>Tabs 的文档</p>
-  </div>
+  <div>Tabs 示例</div>
+  <h1>示例 1</h1>
+  <Tabs>
+    <Tab title="导航 1">内容 1</Tab>
+    <Tab title="导航 2">内容 2</Tab>
+  </Tabs>
 </template>
+<script lang="ts">
+import Tabs from '../lib/Tabs.vue'
+import Tab from '../lib/Tab.vue'
+export default {
+  components: {Tabs, Tab}
+}
+</script>
