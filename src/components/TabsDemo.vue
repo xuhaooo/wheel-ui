710b39ed588d1ff6566121e6599cd397260108a8
xuhao <bingoxuhao@gmail.com>
2021-09-28 17:54:29 +0800

    使用 CSS 切换内容

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,15 +1,20 @@
 <template>
   <div>Tabs 示例</div>
   <h1>示例 1</h1>
-  <Tabs>
-    <Tab title="导航 1">内容 1</Tab>
-    <Tab title="导航 2">内容 2</Tab>
+  <Tabs v-model:selected="x">
+    <Tab title="导航1">内容 1</Tab>
+    <Tab title="导航2">内容 2</Tab>
   </Tabs>
 </template>
 <script lang="ts">
 import Tabs from '../lib/Tabs.vue'
 import Tab from '../lib/Tab.vue'
+import { ref } from 'vue'
 export default {
-  components: {Tabs, Tab}
+  components: {Tabs, Tab},
+  setup(){
+    const x = ref('导航1')
+    return {x}
+  }
 }
 </script>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,16 +1,28 @@
 <template>
   <div class="cat-tabs">
     <div class="cat-tabs-nav">
-      <div class="cat-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
+      <div class="cat-tabs-nav-item" 
+        :class="{selected: t === selected}"
+        v-for="(t,index) in titles"
+        @click="select(t)"
+        :key="index">{{t}}</div>
     </div>
     <div class="cat-tabs-content">
-      <component class="cat-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index"/>
+      <component class="cat-tabs-content-item" 
+        v-for="(c,index) in defaults"
+        :class="{selected: c.props.title === selected}" 
+        :is="c" :key="index"/>
     </div>
   </div>
 </template>
 <script lang="ts">
 import Tab from './Tab.vue'
 export default {
+  props: {
+    selected: {
+      type: String
+    }
+  },
   setup(props, context){
     const defaults = context.slots.default()
     defaults.forEach((tag)=>{
@@ -21,7 +33,10 @@ export default {
     const titles = defaults.map((tag)=>{
       return tag.props.title
     })
-    return {defaults, titles}
+    const select = (title: string) => {
+      context.emit('update:selected', title)
+    }
+    return {defaults, titles, select}
   }
 }
 </script>
@@ -53,6 +68,12 @@ $border-color: #d9d9d9;
   }
   &-content {
     padding: 8px 0;
+    &-item {
+      display: none;
+      &.selected {
+        display: block;
+      }
+    }
   }
 }
 </style>
