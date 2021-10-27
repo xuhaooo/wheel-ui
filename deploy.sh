364a5655369ff09d8800cb41b216140bb3c7376a
xuhao <bingoxuhao@gmail.com>
2021-10-27 21:58:04 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,12 @@ yarn build &&
 cd dist &&
 git init &&
 git add . &&
-git commit -m "update" &&
+git commit -m deploy &&
 git branch -M main &&
 git remote add origin git@github.com:xuhaooo/lion-ui-website.git &&
+git remote add gitee git@gitee.com:bingoxuhao/lion-ui-website.git &&
 git push -f -u origin main &&
+git push -f gitee main &&
 cd -
 echo https://xuhaooo.github.io/lion-ui-website/index.html
+echo https://bingoxuhao.gitee.io/lion-ui-website
